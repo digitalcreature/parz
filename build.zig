130affa16256865bcdbe1f3d4ce425ec84db47f6
sam lovelace <tgrehawi@gmail.com>
2021-05-31 16:17:54 -0400

    set up zigmod proper

--- build.zig
+++ build.zig
@@ -1,9 +1,10 @@
 const Builder = @import("std").build.Builder;
-
+const deps = @import("deps.zig");
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("parz", "src/main.zig");
     lib.setBuildMode(mode);
+    deps.addAllTo(lib);
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
